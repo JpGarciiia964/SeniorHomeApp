c2eebdd68ab78c4a1fdafb87919fb579dec46b1e
jpgarciiia964 <jpgarcia964@gmail.com>
2021-12-02 23:12:09 -0400

    portal informativo

--- src/app/blog/blog.page.ts
+++ src/app/blog/blog.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { LoadingController } from '@ionic/angular';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-blog',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.page.scss'],
 })
 export class BlogPage implements OnInit {
+  helpUrl: any;
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer, public loadingController: LoadingController) { }
 
   ngOnInit() {
+
+    this.presentLoading();
+    this.helpUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://residencia-bethania.negocio.site/');    
   }
+
+  async presentLoading() {
+    const loading = await this.loadingController.create({      
+      message: 'Por favor espere...',
+      duration: 3000
+    });
+    await loading.present();
+    }
 
 }
